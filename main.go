380ec84175e6774c8b661e59ea9463ea8c5d504c
Brian Lee <brianjleeofcl@gmail.com>
2017-12-09 17:31:18 -0800

    code prints result

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"strings"
 
@@ -10,13 +11,26 @@ import (
 
 func main() {
 	db, err := sql.Open("postgres", "dbname=testdb sslmode=disable")
-	rows, err := db.Query("select * from users")
+	rows, err := db.Query("select first_name, last_name, phone_number from users")
 	columns, err := rows.Columns()
 
 	columnHeader := strings.Join(columns, ", ")
 
 	println(columnHeader)
 	println(strings.Repeat("-", len(columnHeader)))
+
+	for rows.Next() {
+		var firstName string
+		var lastName string
+		var phoneNumber string
+		err := rows.Scan(&firstName, &lastName, &phoneNumber)
+
+		fmt.Printf("%s %s: %s\n", firstName, lastName, phoneNumber)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	if err != nil {
 		log.Fatal(err)
